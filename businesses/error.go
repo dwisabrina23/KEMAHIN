011ec903f4c15e65a7f7e833838e242bd781ea54
dwisabrina23 <dwisabrina10@gmail.com>
2021-10-08 02:51:21 +0800

    add new error message

--- businesses/error.go
+++ businesses/error.go
@@ -8,4 +8,8 @@ var (
 	ErrUserNotFound            = errors.New("cannot find user")
 	ErrInternalServer          = errors.New("something went wrong")
 	ErrIDNotFound              = errors.New("id not found")
+	ErrInvalidEventID          = errors.New("invalid order id")
+	ErrOutOfStock              = errors.New("ticket out of stock")
+	ErrInvalidPaymentMeth      = errors.New("invalid payment method")
+	ErrOrderNotFound           = errors.New("order not found")
 )
